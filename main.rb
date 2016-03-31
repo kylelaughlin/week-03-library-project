bbf3d553db30a8318b3d9e5dbea84b06ea9cc802
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-31 11:14:58 -0500

    Add main menu to driver

--- main.rb
+++ main.rb
@@ -4,5 +4,24 @@ require "pry"
 require "yaml"
 require_relative "./lib/library.rb"
 
+def main_menu
+  puts "\n\n--- Library Manager Main Menu ---\n\n"
+  print "Please select one of the following options:\n\n1. Library Branches\n\n >>"
+  selection = gets.chomp.to_i
+  while !([1].include? selection)
+    print "That is an invalid selection please select an option from above.\n\n>>"
+    selection = gets.chomp.to_i
+  end
+  case selection
+  when 1
+    libraries_menu
+  else
+    puts "Something broke - Main menu selection"
+  end
 
-binding.pry
+end
+
+main_menu
+
+
+#binding.pry
