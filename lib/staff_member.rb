6434ad40f046a708a2ff64906d54d0f72d8cb3cc
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-31 13:29:32 -0500

    Clean up comment spacing in staff_member.rb

--- lib/staff_member.rb
+++ lib/staff_member.rb
@@ -2,7 +2,7 @@
 # Staff Member class for staff member objects
 # + name: string with the name of the staff member
 # + email: string with the email of the staff member
-
+#
 class StaffMember < ActiveRecord::Base
 
   validates :name, presence: true
