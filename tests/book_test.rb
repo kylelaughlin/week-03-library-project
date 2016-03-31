4354f60a59d4f1b46030da2f08a13a999c8ab7b6
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-31 14:08:02 -0500

    Add validation tests to book_test.rb

--- tests/book_test.rb
+++ tests/book_test.rb
@@ -8,4 +8,34 @@ class BookTest < Minitest::Test
     refute_nil(b, "Should be an object updon initialization")
   end
 
+  def test_title_validation
+    b = Book.new(title: "The Book of Ruby", author: "Ruby Rails", isbn: 1234567890123)
+    assert(b.valid?, "Should be valid book upon initialization")
+
+    b.title = nil
+    refute(b.valid?, "Should be invalid becuase title can't be nil")
+
+    b.title = ""
+    refute(b.valid?, "Should be invalid becuase title can't be blank")
+  end
+
+  def test_author_validation
+    b = Book.new(title: "The Book of Ruby", author: "Ruby Rails", isbn: 1234567890123)
+    assert(b.valid?, "Should be valid book upon initialization")
+
+    b.author = nil
+    refute(b.valid?, "Should be invalid becuase author can't be nil")
+
+    b.author = ""
+    refute(b.valid?, "Should be invalid becuase author can't be blank")
+  end
+
+  def test_isbn_validation
+    b = Book.new(title: "The Book of Ruby", author: "Ruby Rails", isbn: 1234567890123)
+    assert(b.valid?, "Should be valid book upon initialization")
+
+    b.isbn = nil
+    refute(b.valid?, "Should be invalid becuase isbn can't be nil")
+  end
+  
 end
