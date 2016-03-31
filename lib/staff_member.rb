0ce1aaaf8066b77ac01b1596acb0ba3c3adcbfc0
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-31 13:24:21 -0500

    Add staff member model; create staff member class tests

---
+++ lib/staff_member.rb
@@ -0,0 +1,11 @@
+
+# Staff Member class for staff member objects
+# + name: string with the name of the staff member
+# + email: string with the email of the staff member
+
+class StaffMember < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+end
---
+++ tests/staff_member_test.rb
@@ -0,0 +1,22 @@
+require_relative 'test_helper.rb'
+require_relative '../lib/staff_member.rb'
+
+class StaffMemberTest < Minitest::Test
+
+  def test_create_object_under_normal_circumstances
+    sm = StaffMember.new(name: "Kyle", email: "email@Kyle.com")
+    refute_nil(sm, "Should be an object at initialization")
+  end
+
+  def test_name_valid
+    sm = StaffMember.new(name: "Kyle", email: "email@Kyle.com")
+    assert(sm.valid?, "Should be a valid object at initialization")
+
+    sm.name = nil
+    refute(sm.valid?, "Should be an invalid object as name can't be nil")
+
+    sm.name = ""
+    refute(sm.valid?, "Should be an invalid object as name can't be blank")
+  end
+
+end
